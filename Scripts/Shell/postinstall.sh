59c37e1ac443a33a0e40ac3770b35fa31f542b89
Prasanjit Singh <nixgurus@gmail.com>
2017-06-13 19:16:29 +0530

    //Postinstall errors fixed

--- Scripts/Shell/postinstall.sh
+++ Scripts/Shell/postinstall.sh
@@ -8,8 +8,8 @@ docker exec -i -t mydb mysql -proot -uroot < ../Icinga/IcingaDB.sql
 cd /tmp/
 rm -rf monitoring-plugins monitoring-plugins-2.1.2
 tar xzvf monitoring-plugins-2.1.2.tar.gz ; cd monitoring-plugins-2.1.2/
-./configure --prefix=/tmp/ && make && make install
+./configure --prefix=/tmp/ && make && make install > /dev/null 2>&1
 cd /tmp/libexec
 find . -print | cpio -dumpv /usr/lib64/nagios/plugins
 icinga -d /etc/icinga/icinga.cfg > /dev/null 2>&1
-icinga --show-scheduling /etc/icinga/icinga.cfg
+echo "Icinga Post Install Tasks Completed!"

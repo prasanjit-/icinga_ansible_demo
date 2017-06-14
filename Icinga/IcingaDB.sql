a68b1c8314a238269c652a8f691cb53745f2c26b
Prasanjit Singh <nixgurus@gmail.com>
2017-06-15 02:28:34 +0530

    //WIP

--- Icinga/IcingaDB.sql
+++ Icinga/IcingaDB.sql
@@ -3,6 +3,7 @@ CREATE DATABASE icinga;
 GRANT USAGE ON icinga.* TO 'icinga'@'localhost' IDENTIFIED BY 'icinga' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0;
 GRANT SELECT, INSERT, UPDATE, DELETE, DROP, CREATE VIEW, INDEX, EXECUTE ON icinga.* TO 'icinga'@'localhost';
 GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'root';
+GRANT ALL ON *.* to icinga@'%' IDENTIFIED BY 'icinga';
 FLUSH PRIVILEGES;
 -- --------------------------------------------------------
 -- mysql.sql
